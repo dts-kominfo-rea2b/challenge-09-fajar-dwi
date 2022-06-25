7625e8a400973d99795c4d3e273964942e1b745a
Fajar <fajardwi@its.ac.id>
2022-06-25 23:36:21 +0700

    Assign Challenge

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
+import './Header.css';
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
+
     return (
-        <></>
+        <div style={{width:'100%'}}>
+            <table style={{width:'100%'}}>
+                <tr>
+                    <td><h1>{ props.title }</h1></td>
+                </tr>
+                <tr>
+                    <td><p>{ props.subtitle }</p></td>
+                </tr>
+                <tr>
+                    <td><hr className="hrStyle"/></td>
+                </tr>
+            </table>
+        </div>
     )
 }
 
